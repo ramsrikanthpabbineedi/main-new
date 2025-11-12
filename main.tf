62391b72f74bdcdab7ea01e8b92fde629db3a9d6
ram srikanth pabbineedi <ramsrikanthpabbineedi@gmail.com>
2025-11-12 22:12:41 +0530

    pp

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_instance" "server" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.sub.id
   vpc_security_group_ids = [aws_security_group.sss.id]
-  key_name = aws_key_pair.ram.key_name
+  key_name               = aws_key_pair.ram.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
 }
