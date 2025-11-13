9f59ecd216ad524af58cd2e975e807011468d6b7
ram srikanth pabbineedi <ramsrikanthpabbineedi@gmail.com>
2025-11-13 18:27:37 +0530

    ram

--- main.tf
+++ main.tf
@@ -75,11 +75,11 @@ resource "aws_iam_role_policy_attachment" "example" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ram-ec2"
+  name = "tttt"
   role = aws_iam_role.r-1.name
 }
 resource "aws_key_pair" "ram" {
-  key_name   = "kavaya"
+  key_name   = "ttt"
   public_key = file("${path.module}/ram.pub")
   lifecycle {
     ignore_changes = [public_key]
